841a26dc0da937b7485da221a88f79cb2ce199bc
leifbin <42396536+leifbin@users.noreply.github.com>
2023-02-20 11:31:26 +0800

    8

--- src/pipelines.groovy
+++ src/pipelines.groovy
@@ -1,34 +1,25 @@
-def call(){
+def call(int buildNumber) {
+  if (buildNumber % 2 == 0) {
     pipeline {
-        agent {label 'node-1'}    
-
-        
-        stage('build') {
-            steps {
-                sh '''
-                    # git submodule 构建方式构建方式
-                    # git submodule add git@ph-gitlab.vipsroom.net:ebet_back/ebet_common_core.git core/common
-                    # cd core/common
-                    # git submodule init
-                    # git submodule update
-                    # cd ../../
-                    
-                    # git 私库构建
-                    #rm -rf go.mod go.sum
-                    #/usr/local/go/bin/go clean modcache
-                    #/usr/local/go/bin/go mod init settlement_go
-                    #/usr/local/go/bin/go mod tidy
-                    #/usr/local/go/bin/go build -o $go_name main.go
-                    #rm -rf ${tarName}
-                    gitdes=`git describe --all`
-                    gitfm=`git show -s --format=%H`
-                    gitfom=`git show -s --format=%s`
-                    echo $gitdes > version.txt
-                    echo $gitfm >> version.txt
-                    echo $gitfom >> version.txt
-                    ls
-                '''
-            }
+      agent any
+      stages {
+        stage('Even Stage') {
+          steps {
+            echo "The build number is even"
+          }
         }
+      }
     }
+  } else {
+    pipeline {
+      agent any
+      stages {
+        stage('Odd Stage') {
+          steps {
+            echo "The build number is odd"
+          }
+        }
+      }
+    }
+  }
 }
