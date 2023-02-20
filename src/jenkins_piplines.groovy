4d9ff4fb7c96dc504b4e87218cf5b254b1825a06
leifbin <42396536+leifbin@users.noreply.github.com>
2023-02-20 10:42:38 +0800

    5

---
+++ src/jenkins_piplines.groovy
@@ -0,0 +1,26 @@
+pipeline {
+    agent {label 'node-1'}
+     stages {
+        stage('Checkout') {
+            steps {
+                dir(path: "./") {
+                    git(
+                        branch: "$def_branch",
+                        credentialsId: "004cffe6-ecbb-45da-9b38-c1b7697860cb",	
+                        url: "$giturl",
+                        changelog: true	
+                    )
+                    sh '''
+                        git status
+                        if [[ -n $ver ]];then
+                            git checkout $Build_on_tag
+                            git reset --hard ${ver}
+                        fi
+                        git checkout $Build_on_tag
+                        git branch
+                    '''
+                }
+            }
+        }
+     }
+}
