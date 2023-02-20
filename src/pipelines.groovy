8eeab8aa05460e7c5f8fdfc2284f75802740674e
leifbin <42396536+leifbin@users.noreply.github.com>
2023-02-20 11:35:22 +0800

    10

--- src/pipelines.groovy
+++ src/pipelines.groovy
@@ -1,29 +1,12 @@
-def call() {
-
+def call(int buildNumber) {
     pipeline {
       agent any
       stages {
-        stage('Checkout') {
-            steps {
-                dir(path: "./") {
-                    git(
-                        branch: "$def_branch",
-                        credentialsId: "004cffe6-ecbb-45da-9b38-c1b7697860cb",	
-                        url: "$giturl",
-                        changelog: true	
-                    )
-                    sh '''
-                        git status
-                        if [[ -n $ver ]];then
-                            git checkout $Build_on_tag
-                            git reset --hard ${ver}
-                        fi
-                        git checkout $Build_on_tag
-                        git branch
-                    '''
-                }
-            }
+        stage('Even Stage') {
+          steps {
+            echo "The build number is even"
+          }
         }
       }
     }
-}
+  }
