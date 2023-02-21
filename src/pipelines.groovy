3cb0334466e1ef1283164e3cf07b274efb80d85f
leifbin <42396536+leifbin@users.noreply.github.com>
2023-02-21 12:16:11 +0800

    14

--- src/pipelines.groovy
+++ src/pipelines.groovy
@@ -1,20 +1,38 @@
-def call() {
-    pipeline {
-      agent any
-      stages {
-        stage('Checkout') {
-            steps {
-                dir(path: "./") {
-                    git(
-                        branch: "$def_branch",
-                        credentialsId: "004cffe6-ecbb-45da-9b38-c1b7697860cb",	
-                        url: "$giturl",
-                        changelog: true	
-                    )
+def call(Map map) {
 
+    pipeline {
+        agent  {label map.RUN_NODE }    
+        environment {
+         //   def proj = "${map.PROJ}"
+         //   def tarName = "${map.TAR_NAME}"
+         //   def serviceDir = "${map.Service_Dir}"
+            def def_branch = "${map.DEFAULT_BRANCH}"
+            def GIT_URL = "${map.GIT_URL}" // 主项目地址
+            def ver = "${map.Ver}"
+            def Build_on_tag  = "${map.Build_on_tag}"
+        }
+        stages {
+            stage('Checkout') {
+                steps {
+                    dir(path: "./") {
+                        git(
+                            branch: "${env.def_branch}",
+                            credentialsId: "004cffe6-ecbb-45da-9b38-c1b7697860cb",	
+                            url: "${env.GIT_URL}",
+                            changelog: true	
+                        )
+                        sh '''
+                            git status
+                            if [[ -n ${env.ver} ]];then
+                                git checkout ${env.Build_on_tag}
+                                git reset --hard ${env.ver}
+                            fi
+                            git checkout ${env.Build_on_tag}
+                            git branch
+                        '''
+                    }
                 }
             }
-        }
-      }
-    }
-  }
+        }    
+    }    
+}
