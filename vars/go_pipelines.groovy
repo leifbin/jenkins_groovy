e5fd2ad08cc7388cd89d106a6a431b9ae972157a
leifbin <42396536+leifbin@users.noreply.github.com>
2023-02-22 11:38:55 +0800

    39

--- vars/go_pipelines.groovy
+++ vars/go_pipelines.groovy
@@ -15,18 +15,7 @@ def call(Map map) {
             def go_name = "${proj}"
         }
 
-        parameters {
-            gitParameter (branch:'',
-            branchFilter: 'origin/(.*)',
-            defaultValue:  env.def_branch,
-            description: '选择将要构建的分支',
-            name: 'Build_on_tag',
-            quickFilterEnabled: true, 
-            selectedValue: 'TOP',
-            sortMode: 'DESCENDING_SMART',
-            tagFilter: '*',
-            type: 'PT_BRANCH_TAG',
-            useRepository: env.GIT_URL)
+         gitParameter (branch:'', branchFilter: 'origin/(.*)', defaultValue:  env.def_branch, description: '选择将要构建的分支', name: 'Build_on_tag', quickFilterEnabled: true, selectedValue: 'TOP', sortMode: 'DESCENDING_SMART', tagFilter: '*', type: 'PT_BRANCH_TAG', useRepository: env.GIT_URL)
             }
         stages {
             
