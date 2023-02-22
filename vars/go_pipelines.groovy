5e45bab8d4d5073b1173e68f02538e89fd1a7309
leifbin <42396536+leifbin@users.noreply.github.com>
2023-02-22 12:38:09 +0800

    43

--- vars/go_pipelines.groovy
+++ vars/go_pipelines.groovy
@@ -1,5 +1,4 @@
 def call(Map map) {
-
     pipeline {
         agent {
             label map.RUN_NODE
@@ -16,45 +15,45 @@ def call(Map map) {
         }
 
         parameters {
-            gitParameter (branch:env.def_branch, branchFilter: 'origin/(.*)', defaultValue:  env.def_branch, description: '选择将要构建的分支', name: 'Build_on_tag', quickFilterEnabled: true, selectedValue: 'TOP', sortMode: 'DESCENDING_SMART', tagFilter: '*', type: 'PT_BRANCH_TAG', useRepository: env.GIT_URL)
-            }
+            gitParameter(branch:env.def_branch, branchFilter: 'origin/(.*)', defaultValue:  env.def_branch, description: '选择将要构建的分支', name: 'Build_on_tag', quickFilterEnabled: true, selectedValue: 'TOP', sortMode: 'DESCENDING_SMART', tagFilter: '*', type: 'PT_BRANCH_TAG', useRepository: ${env.GIT_URL})
+        }
         stages {
-            
             stage('Checkout') {
                 steps {
-                    dir(path: "./") {
+                    dir(path: './') {
                         git(
                             branch: "${env.DEF_BRANCH}",
-                            credentialsId: "004cffe6-ecbb-45da-9b38-c1b7697860cb",	
+                            credentialsId: '004cffe6-ecbb-45da-9b38-c1b7697860cb',
                             url: "${env.GIT_URL}",
-                            changelog: true	
+                            changelog: true
                         )
-                    script {    
-                       echo '开始拉去代码' 
-                    sh '''
-                        git status
-                        if [[ -n $ver ]];then
-                            git checkout env.Build_on_tag
-                            git reset --hard ${ver}
-                        fi
-                        git checkout $Build_on_tag
-                        git branch
-                        echo  $Build_on_tag
-                    ''' 
-                        echo '拉取代码结束'     
-                            }   
+                        script {
+                            echo '开始拉去代码'
+                            echo ${env.GIT_URL}
+                            sh '''
+                                    git status
+                                    if [[ -n $ver ]];then
+                                        git checkout env.Build_on_tag
+                                        git reset --hard ${ver}
+                                    fi
+                                    git checkout $Build_on_tag
+                                    git branch
+                                    echo  $Build_on_tag
+                                '''
+                            echo '拉取代码结束'
+                        }
                     }
                 }
             }
 
             stage('build') {
                 steps {
-                sh '''
+                    sh '''
                     # git 私库构建
                     /usr/local/go/bin/go mod tidy
                     /usr/local/go/bin/go build -o $go_name main.go
                     rm -rf ${tarName}
-                    
+
                     #start写入版本信息
                     gitdes=`git describe --all`
                     gitfm=`git show -s --format=%H`
@@ -63,23 +62,20 @@ def call(Map map) {
                     echo $gitfm >> version.txt
                     echo $gitfom >> version.txt
                     #end写入版本信息
-                    
+
                     pwd
                 '''
+                }
             }
-        }
             stage('deploy') {
                 steps {
-                sh '''
+                    sh '''
                     cd ..
-                    
+
                     tar -zcvf $tarName -C $JOB_NAME . | xargs -n 5
                 '''
+                }
             }
         }
-        
     }
-
-    
-    }    
 }
